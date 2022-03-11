a5c028982e2a89482e485df10e84d75b271d4748
northyear <zijz@qq.com>
2022-03-10 23:02:32 -0500

    Refactor: Set preview_cutoff for telescope

--- DP_run.py
+++ DP_run.py
@@ -3,13 +3,13 @@ import numpy as np
 from DP import DPGMM
 
 
-y = np.append(np.random.normal(loc = 16, size = 50), np.random.normal(loc = 8, size = 50))
-a = DPGMM(y = y,
+y = np.append(np.random.normal(loc=16, size=50),
+              np.random.normal(loc=8, size=50))
+a = DPGMM(y=y,
           sigma2=1.0, m0=2.5, D=1.0, a=1.0, b=1.0, c=1.0, d=1.0,
           initial_components=2, iters=5000)
 a.initialize()
-s, theta = a.sample()
-
+hypers, s, theta = a.sample()
 
 
 # %%
